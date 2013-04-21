11409ac14048f811c14719ace45549b8a81187aa
Robert Szpanderfer <rszpand1@umbc.edu>
2013-04-21 18:43:02 -0400

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
   subject { page }
-
+  
   describe "index" do
 
     let(:user) { FactoryGirl.create(:user) }
 
-    before(:each) do
+    before(:all) { 30.times { FactoryGirl.create(:user) } }
+    after(:all) { User.delete_all }
+
+    before do
       sign_in user
       visit users_path
     end
@@ -17,9 +20,6 @@ describe "UserPages" do
     it { should have_selector('h1', text: 'All users') }
 
     describe "pagination" do
-      
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
-      after(:all) { User.delete_all }
 
       it { should have_selector('div.pagination') }
 
@@ -29,13 +29,13 @@ describe "UserPages" do
         end
       end
     end
-
+    
     describe "delete links" do
-      
+
       it { should_not have_link('delete') }
 
       describe "as an admin user" do
-        let(:admin) {FactoryGirl.create(:admin) }
+        let(:admin) { FactoryGirl.create(:admin) }
         before do
           sign_in admin
           visit users_path
@@ -49,23 +49,33 @@ describe "UserPages" do
       end
     end
   end
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
 
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_expected_header('Sign up') }
-    it { should have_expected_title('Sign up') }
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('title', text: 'Sign up') }
   end
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_expected_header(user.name) }
-    it { should have_expected_title(user.name) }
-  end
-
+  
   describe "signup" do
+
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -74,35 +84,31 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-
-      describe "after submission" do
-        before { click_button submit }
-
-        it { should have_expected_title('Sign up') }
-        it { should have_content('error') }
-        it { should_not have_content('Password digest') }
-      end
     end
 
     describe "with valid information" do
-
-      let(:user) { FactoryGirl.build(:exampleuser) }
-      before { valid_signup(user) }
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-
-      describe "after saving a user" do
+      
+      describe "after saving the user" do
         before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
 
-        it { should have_expected_title(user.name) }
-        it { should have_success_message('Welcome') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
       end
     end
   end
-
+  
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
@@ -113,9 +119,15 @@ describe "UserPages" do
     describe "page" do
       it { should have_selector('h1', text: "Update your profile") }
       it { should have_selector('title', text: "Edit user") }
-      it { should have_link('change', href: "http://gravatar.com/emails") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
 
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+    
     describe "with valid information" do
       let(:new_name) { "New Name" }
       let(:new_email) { "new@example.com" }
