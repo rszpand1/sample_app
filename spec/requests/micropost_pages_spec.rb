bfa05a1283cabf57dc4d306c01ae926684adbccd
Robert Szpanderfer <rszpand1@umbc.edu>
2013-04-21 20:57:38 -0400

    Finish Chapter 10 exercises

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -2,44 +2,56 @@ require 'spec_helper'
 
 describe "Micropost pages" do
 
-  subject { page }
+   subject { page }
 
-  let(:user) { FactoryGirl.create(:user) }
-  before { sign_in user }
+   let(:user) { FactoryGirl.create(:user) }
+   before { sign_in user }
 
-  describe "micropost creation" do
+   describe "micropost creation" do
     before { visit root_path }
 
-    describe "with invalid information" do
 
+    describe "with invalid information" do
+ 
       it "should not create a micropost" do
-        expect { click_button "Post" }.not_to change(Micropost, :count)
+         expect { click_button "Post" }.not_to change(Micropost, :count)
       end
 
       describe "error messages" do
         before { click_button "Post" }
-        it { should have_content('error') } 
+        it { should have_content('error') }
       end
     end
+
 
     describe "with valid information" do
-
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do
-        expect { click_button "Post" }.to change(Micropost, :count).by(1)
+         expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
     end
   end
-  
-  describe "micropost destruction" do
-    before { FactoryGirl.create(:micropost, user: user) }
 
-    describe "as correct user" do
-      before { visit root_path }
+    describe "micropost destruction" do
+      before { FactoryGirl.create(:micropost, user: user) }
 
-      it "should delete a micropost" do
-        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+       describe "as correct user" do
+        before { visit root_path }
+
+        it "should delete a micropost" do
+           expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+        end
       end
-    end
-  end
+   end
+
+     describe "delete links" do
+
+         let(:user) { FactoryGirl.create(:user) }
+         let(:current_user) { FactoryGirl.create(:user) }
+
+         describe "as incorrect user" do
+           before { visit root_path }
+           it { should_not have_link('delete', href: user_path(user)) }
+         end
+      end
 end
