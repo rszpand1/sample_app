a464c5d5fad8416bee38ae48b41eece7a42da1f1
Robert Szpanderfer <rszpand1@umbc.edu>
2013-04-28 17:42:07 -0400

    Finish Chapter 11 Exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+# id :integer not null, primary key
+# name :string(255)
+# email :string(255)
+# created_at :datetime not null
+# updated_at :datetime not null
 #
 
 require 'spec_helper'
@@ -14,16 +14,27 @@ require 'spec_helper'
 describe User do
 
   before do
-   @user = User.new(name: "Example User", email: "user@example.com",
-                    password: "foobar", password_confirmation: "foobar")
+    @user = User.new :name => "Example User", :email => "user@example.com",
+                       :password => "foobar", :password_confirmation => "foobar"
+
   end
 
   subject { @user }
+
+  describe "admin attribute" do
+    it "should not be accessible" do
+        expect do
+            @user.update_attributes(:admin => true)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
@@ -41,33 +52,33 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
 
-  describe "accessible attributes" do
-    it "should not allow access to admin" do
-      expect do
-        User.new(admin: "1")
-      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
+  describe "with admin attribute set to 'true'" do
+     before do
+      @user.save!
+      @user.toggle!(:admin)
+     end
+
+     it { should be_admin }
   end
 
-  describe "with admin attribute set to 'true'" do
-    before { @user.toggle!(:admin) }
-
-    it { should be_admin }
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
   describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
+     before { @user.name = " " }
+     it { should_not be_valid }
   end
 
   describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
+     before { @user.email = " " }
+     it { should_not be_valid }
   end
 
   describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
+     before { @user.name = "a" * 51 }
+     it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
@@ -75,19 +86,19 @@ describe User do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
 foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
+         @user.email = invalid_address
+         @user.should_not be_valid
       end
     end
   end
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end
+       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+       addresses.each do |valid_address|
+          @user.email = valid_address
+          @user.should be_valid
+       end
     end
   end
 
@@ -97,28 +108,27 @@ foo@bar_baz.com foo@bar+baz.com]
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-
-    it { should_not be_valid }
+  it { should_not be_valid }
   end
 
   describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
+    before { @user.password = " " }
+      it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
+      it { should_not be_valid }
   end
 
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
-    it { should_not be_valid }
+     it { should_not be_valid }
   end
 
-  describe "when password is too short" do
+  describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should_not be_valid }
+      it { should be_invalid }
   end
 
   describe "return value of authenticate method" do
@@ -129,83 +139,101 @@ foo@bar_baz.com foo@bar+baz.com]
       it { should == found_user.authenticate(@user.password) }
     end
 
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-      
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
-    end
   end
-
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
+ end
 
   describe "micropost associations" do
-    
+
     before { @user.save }
     let!(:older_micropost) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
     let!(:newer_micropost) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
     it "should have the right microposts in the right order" do
-      @user.microposts.should == [newer_micropost, older_micropost]
+       @user.microposts.should == [newer_micropost, older_micropost]
     end
 
     it "should destroy associated microposts" do
-      microposts = @user.microposts
-      @user.destroy
-      microposts.each do |micropost|
-        Micropost.find_by_id(micropost.id).should be_nil
-      end
-    end
+       microposts = @user.microposts.dup
+       @user.destroy
+       microposts.should_not be_empty
+       microposts.each do |micropost|
+         Micropost.find_by_id(micropost.id).should be_nil
+     end
+   end
 
     describe "status" do
       let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
       let(:followed_user) { FactoryGirl.create(:user) }
 
       before do
-        @user.follow!(followed_user)
-        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+         @user.follow!(followed_user)
+         3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
-      
-      its(:feed) { should include(older_micropost) }
+
+
       its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
       its(:feed) do
         followed_user.microposts.each do |micropost|
-          should include(micropost)
-        end
+        should include(micropost)
       end
-    end
-  end
+     end
+   end
+ end
 
-  describe "following" do
-    let(:other_user) { FactoryGirl.create(:user) }
-    before do
-      @user.save
-      @user.follow!(other_user)
-    end
+    describe "following" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        @user.save
+        @user.follow!(other_user)
+      end
 
-    it { should be_following(other_user) }
-    its(:followed_users) { should include(other_user) }
+      it { should be_following(other_user) }
+      its(:followed_users) { should include(other_user) }
 
-    describe "followed user" do
-      subject { other_user }
-      its(:followers) { should include(@user) }
-    end
+      describe "followed user" do
+        subject { other_user }
+        its(:followers) { should include(@user) }
+      end
+   
+      describe "and unfollowing" do
+        before { @user.unfollow!(other_user) }
 
-    describe "and unfollowing" do
-      before { @user.unfollow!(other_user) }
+        it { should_not be_following(other_user) }
+        its(:followed_users) { should_not include(other_user) }
+      end
+   end
 
-      it { should_not be_following(other_user) }
-      its(:followed_users) { should_not include(other_user) }
-    end
+    describe "relationship associations" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        @user.save
+        @user.follow!(other_user)
+        other_user.follow!(@user)
+      end
+
+      it "should destroy associated relationships" do
+        relationships = @user.relationships
+        @user.destroy
+        relationships.should be_empty
+      end
+
+      it "should destroy associated reverse relationships" do
+        reverse_relationships = @user.reverse_relationships
+        @user.destroy
+        reverse_relationships.should be_empty
+     end
   end
 end
